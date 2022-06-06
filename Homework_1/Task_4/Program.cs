84d666430527fec5823523c64c5babc0452ac927
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 22:43:23 +0300

    task 4 is done

--- Homework_1/Task_4/Program.cs
+++ Homework_1/Task_4/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// all even from 1 to N
+Console.Write("Insert a number: ");
+string N_str = Console.ReadLine();
+int N = int.Parse(N_str);
+int i = 2;
+while (i<N-1){
+    Console.Write($"{i}, ");
+    i+=2;
+}
+Console.Write(i);
