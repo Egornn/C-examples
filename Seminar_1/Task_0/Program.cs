91b7de86bb67dc7332afcb1b63eca000e824df18
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 20:29:33 +0300

    if els

--- Seminar_1/Task_0/Program.cs
+++ Seminar_1/Task_0/Program.cs
@@ -4,5 +4,13 @@ Console.Write ("Number to square: ");
 string number = Console.ReadLine();
 double answer = double.Parse(number);
 answer = answer * answer;
-Console.WriteLine("Square of your number is " + answer);
+//Console.WriteLine("Square of your number is " + answer);
 Console.WriteLine($"Square of {number} is {answer}");
+if (answer>=25)
+{
+    Console.WriteLine("Result is grater or equal than 25");
+}
+else
+{
+    Console.WriteLine("Result is less than 25");
+}
