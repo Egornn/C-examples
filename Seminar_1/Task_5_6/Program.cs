83cebd11af131f1352181365e4e4d14bf8039555
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 22:14:47 +0300

    Task 5 & task 6 with *

---
+++ Seminar_1/Task_5_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// модуль
+Console.Write("Number to get absolute: ");
+string number_str = Console.ReadLine();
+double number = double.Parse (number_str);
+if (number <0) {number=-number;}
+Console.WriteLine($"Absolute value of {number_str} is {number}");
+// обратимость
+
+Console.Write("Number to get inverse: ");
+string inverse_str = Console.ReadLine();
+double inverse = double.Parse (inverse_str);
+inverse = 1/inverse;
+Console.WriteLine($"The inverse of a {inverse_str} is {inverse}");
