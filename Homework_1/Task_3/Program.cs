a8fc7a4c5d812e9dc39de981b5d78ab515193168
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 22:38:13 +0300

    task 3 complete

--- Homework_1/Task_3/Program.cs
+++ Homework_1/Task_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Even test
+Console.Write("Insert a number: ");
+string number_str = Console.ReadLine();
+int number = int.Parse(number_str);
+if (number%2==0) {
+    Console.WriteLine($"{number_str} is even");
+}
+else {
+    
+    Console.WriteLine($"{number_str} is odd");
+}
