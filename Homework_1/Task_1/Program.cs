fcbff8b58bf7b422f949ed8eefd8b8ecae700544
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 22:27:27 +0300

    first task

---
+++ Homework_1/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Greater and leeser number out of 2
+Console.Write("Insert the first number: ");
+string first_str = Console.ReadLine();
+Console.Write("Insert the second number: ");
+string second_str = Console.ReadLine();
+
+double first = double.Parse(first_str);
+double second = double.Parse(second_str);
+
+if (first>second) {Console.WriteLine($"The greater one is {first} and the lesser one is {second}");}
+else{Console.WriteLine($"The greater one is {second} and the lesser one is {first}");}
