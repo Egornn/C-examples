586fcf78e1e8b7a89790994ab5cce18500b24bad
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 19:25:02 +0300

    If-else

---
+++ Example_005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Username");
+string username = Console.ReadLine ();
+if (username.ToLower() == "egor")
+{
+    Console.WriteLine("Hooray, it's Egor");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
