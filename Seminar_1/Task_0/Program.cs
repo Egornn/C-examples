0ad40449f1a888d0c0007f7f22ecda19863c928c
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 20:19:43 +0300

    square

---
+++ Seminar_1/Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Takes number, output its square
+
+Console.Write ("Number to square: ");
+string number = Console.ReadLine();
+double answer = double.Parse(number);
+answer = answer * answer;
+Console.WriteLine("Square of your number is " + answer);
