fdbde97561743e2ae2390b31b6a0a4b1abdf7cbc
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-06 22:34:40 +0300

    second task

---
+++ Homework_1/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Greater and leeser number out of 3
+Console.Write("Insert the first number: ");
+string first_str = Console.ReadLine();
+Console.Write("Insert the second number: ");
+string second_str = Console.ReadLine();
+Console.Write("Insert the third number: ");
+string third_str = Console.ReadLine();
+
+
+double first = double.Parse(first_str);
+double second = double.Parse(second_str);
+double third = double.Parse(third_str);
+
+double max = first;
+
+if (second>max) {max=second;}
+if (third>max) {max=third;}
+
+Console.WriteLine($"The greater one is {max}");
