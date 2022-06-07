7f7db420a5631d46ab8e1df502f0cd2a0f0ed8e7
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-07 19:23:49 +0300

    first task

---
+++ Lesson_2/task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int a1 = 11;
+int b1 = 12;
+int c1 = 13;
+int a2 = 113;
+int b2 = 122;
+int c2 = 113;
+int a3 = 511;
+int b3 = 142;
+int c3 = 123;
+
+int max=a1;
+if (a1>max) {max=a1;}
+if (b1>max) {max=b1;}
+if (c1>max) {max=c1;}
+if (a2>max) {max=a2;}
+if (b2>max) {max=b2;}
+if (c2>max) {max=c2;}
+if (a3>max) {max=a3;}
+if (b3>max) {max=b3;}
+if (c3>max) {max=c3;}
+Console.WriteLine(max);
