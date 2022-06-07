f76c0830878b0500289243d20543aec91ef1c5d9
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-07 22:41:45 +0300

    first recursice solution

--- Dynamic_programming/Fibonachi/Program.cs
+++ Dynamic_programming/Fibonachi/Program.cs
@@ -1,2 +1,18 @@
-﻿
+﻿int fib(int n)
+{
+    if (n == 1 ) { return 0; }
+    int counter = 2;
+    int FibonacciPrevious = 0;
+    int FibonacciCurrent = 1;
+    int temp =0;
+    while (counter < n)
+    {
+        temp=FibonacciCurrent;
+        FibonacciCurrent=FibonacciCurrent+FibonacciPrevious;
+        FibonacciPrevious=temp;
+        counter++;
+    }
+    return FibonacciCurrent;
+}
 
+Console.WriteLine( fib(10));
