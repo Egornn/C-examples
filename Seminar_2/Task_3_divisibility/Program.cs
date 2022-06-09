09096643e9005bd5d26ca2e8420ebc37fd11b64c
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 21:17:27 +0300

    divisibility

---
+++ Seminar_2/Task_3_divisibility/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Input Divider and Divisor");
+string DividerStr = Console.ReadLine();
+string DivisorStr = Console.ReadLine();
+int divider = int.Parse(DividerStr);
+int divisor = int.Parse(DivisorStr);
+
+if (divisor % divider)
+{
+    console.WriteLine("Divisible");
+}
+else
+{
+    console.WriteLine($"Remainder is {divisor % divider} ");
+}
