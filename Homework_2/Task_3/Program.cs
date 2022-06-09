065ba53376d861d437b768b502430a3ce0edf5b6
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-10 00:16:58 +0300

    added a case for the negative numbers

--- Homework_2/Task_3/Program.cs
+++ Homework_2/Task_3/Program.cs
@@ -11,16 +11,24 @@ int power(int foundation, int power)
     }
     return answer;
 }
+int abs(int number)
+{
+    if (number < 0) { number = -number; }
+    return number;
+}
 Console.WriteLine("Input any digit number (preferably more than a 1000)");
 string numStr = Console.ReadLine();
 int number = int.Parse(numStr);
 if (number <= 99 && number >= -99) { Console.WriteLine("There is not enough digits"); }
-int remainder = number;
-int digits = 0;
-while (remainder != 0)
+else
 {
-    digits++;
-    remainder = remainder / 10;
+    int remainder = abs(number);
+    int digits = 0;
+    while (remainder != 0)
+    {
+        digits++;
+        remainder = remainder / 10;
+    }
+    int ThirdDigit = (number / power(10, digits - 3)) % 10;
+    Console.WriteLine($"The third digit of {number} is {ThirdDigit}");
 }
-int ThirdDigit = (number / power(10, digits - 3)) % 10;
-Console.WriteLine($"The third digit of {number} is {ThirdDigit}");
