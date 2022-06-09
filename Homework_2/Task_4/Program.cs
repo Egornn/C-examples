eaeb38d7611a391e63b49e0b10df6bf9cefabe64
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-10 00:11:58 +0300

    simple weekend check

--- Homework_2/Task_4/Program.cs
+++ Homework_2/Task_4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    string text = Console.ReadLine();
+    int number = int.Parse(text);
+    return number;
+}
+
+int num = prompt("Input a number for day of the week");
+if (num < 1 || num > 7) { Console.WriteLine("There is only 7 days in a week"); }
+else
+{
+    if (num < 6) { Console.WriteLine("It is a workday"); }
+    else { { Console.WriteLine("It is a weekend"); } }
+}
