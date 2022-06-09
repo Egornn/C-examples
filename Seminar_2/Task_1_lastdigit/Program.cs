c533f86347a53284d41db105768a9a8d3ed2af35
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 20:33:23 +0300

    last digit

---
+++ Seminar_2/Task_1_lastdigit/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Input 3 digit number");
+string numberStr = Console.ReadLine(); 
+int number = int.Parse(numberStr);
+
+
+while (number<100 || number>=1000) {
+    Console.WriteLine("Число не трехзначное. Повторите ввод");
+    numberStr = Console.ReadLine(); 
+    number = int.Parse(numberStr);
+}
+
+int LastDigit = number % 10;
+Console.WriteLine($"Last digit is {LastDigit}");
+
