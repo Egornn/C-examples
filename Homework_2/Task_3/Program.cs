08e580af2b3477cd20a92cc3e151d472a1c59e93
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-10 00:03:29 +0300

    third digit with my own fucnction of n^k

--- Homework_2/Task_3/Program.cs
+++ Homework_2/Task_3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int power(int foundation, int power)
+{
+    if (power == 0) { return 1; }
+    int counter = 1;
+    int answer = 1;
+    while (counter <= power)
+    {
+        answer = answer * foundation;
+        counter++;
+    }
+    return answer;
+}
+Console.WriteLine("Input any digit number (preferably more than a 1000)");
+string numStr = Console.ReadLine();
+int number = int.Parse(numStr);
+if (number <= 99 && number >= -99) { Console.WriteLine("There is not enough digits"); }
+int remainder = number;
+int digits = 0;
+while (remainder != 0)
+{
+    digits++;
+    remainder = remainder / 10;
+}
+int ThirdDigit = (number / power(10, digits - 3)) % 10;
+Console.WriteLine($"The third digit of {number} is {ThirdDigit}");
