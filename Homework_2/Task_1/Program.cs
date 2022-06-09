bbb868116cf7f3a80768b9636652da4fc14c9da9
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 23:22:49 +0300

    first task is fully complete

--- Homework_2/Task_1/Program.cs
+++ Homework_2/Task_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int prompt (string message){
+    Console.WriteLine(message);
+    string text= Console.ReadLine();
+    int number=int.Parse(text);
+    return number; 
+    }
+
+int num=prompt("Input 3 digit number");
+while (num<100 || num>999){
+    Console.WriteLine("It's not a 3 digit number");
+    num=prompt("Input 3 digit number");
+}
+Console.WriteLine($"The second digit is {(num%100)/10}");
