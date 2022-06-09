881b94ed4cfaca800130ea6a6bb77638b81ae27c
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 21:26:18 +0300

    divisibility by 7 and 23

---
+++ Seminar_2/Task_4_divisibility_by_161/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Input: ");
+string numStr = Console.ReadLine();
+int num = int.Parse(numStr);
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Divisible by 23 and 7");
+}
+else
+{
+    Console.WriteLine("Indivisible by 23 and 7");
+}
