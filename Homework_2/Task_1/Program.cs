19d1775f098aec5cde8d3e41d2d5fa6615aabfc1
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 23:24:08 +0300

    change in format

--- Homework_2/Task_1/Program.cs
+++ Homework_2/Task_1/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int prompt (string message){
+int prompt(string message)
+{
     Console.WriteLine(message);
-    string text= Console.ReadLine();
-    int number=int.Parse(text);
-    return number; 
-    }
-
-int num=prompt("Input 3 digit number");
-while (num<100 || num>999){
-    Console.WriteLine("It's not a 3 digit number");
-    num=prompt("Input 3 digit number");
+    string text = Console.ReadLine();
+    int number = int.Parse(text);
+    return number;
 }
-Console.WriteLine($"The second digit is {(num%100)/10}");
+
+int num = prompt("Input 3 digit number");
+while (num < 100 || num > 999)
+{
+    Console.WriteLine("It's not a 3 digit number");
+    num = prompt("Input 3 digit number");
+}
+Console.WriteLine($"The second digit is {(num % 100) / 10}");
