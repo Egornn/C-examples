e3c1109d716cb2916b45592ba8fc278e6dfa3d30
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-09 23:32:37 +0300

    done with removing second digit

--- Homework_2/Task_2/Program.cs
+++ Homework_2/Task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    string numStr = Console.ReadLine();
+    int num = int.Parse(numStr);
+    return num;
+}
+int number = prompt("Input a 3 digit number");
+while (number <= 99 || number >= 1000)
+{
+    Console.WriteLine("Input is not a 3 digit number");
+    number = prompt("Input a 3 digit number");
+}
+int mod = number / 100 * 10 + number % 10;
+
+Console.WriteLine($"Without a second digit {number} is {mod}");
