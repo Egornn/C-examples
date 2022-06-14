d7b308f5fdb54f5d4b34674a14a7bc43f797cccb
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-14 20:50:33 +0300

    Only 4 quadrants

--- Seminar_3/Task_1/Program.cs
+++ Seminar_3/Task_1/Program.cs
@@ -1,9 +1,22 @@
-﻿// By given quarter return possible x,y range
+﻿// By given quadrants return possible x,y range
 string prompt (string msg)
 {
-   Console.WriteLine (msg);
-   
-   return Console.ReadLine();  
+    Console.WriteLine (msg);  
+    return Console.ReadLine();  
 } 
 
-string quarterStr=prompt("Enter the quarter of interest");
+int quadrants = int.Parse(prompt("Enter the quadrants of interest"));
+string x=">", y=">";
+if (quadrants<1 || quadrants>4){Console.WriteLine ("There are only 4 quadrants");}
+else{
+if (quadrants==2 || quadrants == 3)
+{
+    x="<";
+} 
+if (quadrants==2 || quadrants == 3)
+{
+    y="<";
+} 
+
+Console.WriteLine($"for the quadrants {quadrants} x{x}0 and y{y}0");
+}
