3e9bebf290dc06e4ee7a5ef0a35c3d9bb66f4458
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-15 06:49:29 +0300

    working for given 5 digit

---
+++ Homework_3/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+string prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    string number = System.Console.ReadLine(); 
+    return number;
+}
+string number=prompt("Select a 5-digit number");
+
+
+if (number[0]  == number[4]&& number[1]  == number[3] ) 
+{
+    Console.WriteLine($"{number} is a palindrome");
+}
+else
+{
+    Console.WriteLine($"{number} is not a palindrome");
+}
+
+
