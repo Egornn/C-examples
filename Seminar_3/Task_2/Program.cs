89a207981227a7f1fad3f408631e2b15cdcc66f2
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-15 05:41:31 +0300

    found a comma in coordinates

--- Seminar_3/Task_2/Program.cs
+++ Seminar_3/Task_2/Program.cs
@@ -7,5 +7,16 @@ string prompt(string msg)
 }
 
 string coordinates1 = prompt ("Input coorinates in the form x1, y1");
-string coordinates2 = prompt ("Input coorinates in the form x2, y2");
+//string coordinates2 = prompt ("Input coorinates in the form x2, y2");
 
+int comma = 0;
+int length = coordinates1.Length;
+for (int counter = 0; counter <  length; counter++ )
+{
+    if (coordinates1[counter] == ',') 
+    {
+        comma=counter;
+    }
+    
+}
+Console.WriteLine(comma);
