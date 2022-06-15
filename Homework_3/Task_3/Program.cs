8c455e9b6f6b30dc527c11194bb83ae800daa197
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-15 07:36:01 +0300

    simple cycle

--- Homework_3/Task_3/Program.cs
+++ Homework_3/Task_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+int prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    string N = System.Console.ReadLine(); 
+    return int.Parse(N);
+}
+int powerx(int based, int power)
+{
+    int result=1;
+    for (int i =0; i<power;i++)
+    {
+        result=result*based;
+    }
+    return result;
+}
+int N=prompt("Input N");
+for (int i =1; i<N;i++)
+{
+    Console.Write($"{powerx(i,3)}, ");
+}
+Console.WriteLine (powerx(N,3)+".");
