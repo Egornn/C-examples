fac44c571641ec274078ba51b181f5aa29173787
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-16 22:31:03 +0300

    done with square brackets

--- Homework_4/Task_3/Program.cs
+++ Homework_4/Task_3/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+int promptNumber (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int [] RandomArray (int[] array, int rangeB, int rangeU)
+{
+    int rng = 0;
+    for (int i=0; i<array.Length;i++)
+        {
+        rng= new Random().Next(rangeB,rangeU+1);
+        array[i]=rng;
+        }
+    return array;
+}
+
+void PrintArray (int [] array)
+{
+    Console.Write("[");
+    for (int i=0; i<array.Length-1; i++)
+    {
+        Console.Write(array[i]+", ");
+    }
+    Console.WriteLine(array[array.Length-1]+"]");
+}
+
+
+int size = promptNumber("input the size of the array: ");
+int RangeB = promptNumber("input the lower bound of the array: ");
+int RangeU = promptNumber("input the upper bound of the array: ");
+int[] array = new int[size];
+array=RandomArray(array,RangeB, RangeU);
+PrintArray(array);
+
