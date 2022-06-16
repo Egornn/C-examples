5ec4a4d68d9da1acb116eb4b48d0e5ac57ad38e5
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-16 22:12:23 +0300

    done

--- Homework_4/Task_1/Program.cs
+++ Homework_4/Task_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int promptNumber (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int A = promptNumber("Input the base of power ");
+int B = promptNumber("Input the power ");
+
+int powerNat (int based, int power)
+{
+    int result = based;
+    for (int i = 1; i<power; i++)
+    {
+       result=result*based;
+    }
+    return result;
+}
+Console.WriteLine($"{A} to the power {B} is {powerNat(A, B)}");
