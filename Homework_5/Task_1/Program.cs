e2e569734dca420f4b19011a6113d984a50fe144
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-20 22:56:02 +0300

    done

--- Homework_5/Task_1/Program.cs
+++ Homework_5/Task_1/Program.cs
@@ -12,7 +12,7 @@ int [] RandomArray (int len, int min, int max)
     
     int [] RngArray = new int [len];
     for (int i =0; i<RngArray.Length;i++)
-    RngArray[i]= new Random().Next(min,max);
+    RngArray[i]= new Random().Next(min,max+1);
     return RngArray;
 }
 
@@ -25,3 +25,19 @@ void PrintArray (int [] array)
     }
     Console.WriteLine(array[array.Length-1]+"]");
 }
+
+int NumberOfEven (int [] array)
+{
+    int result=0;
+    for (int i=0; i<array.Length; i++)
+    if (array[i]%2==0) result++;
+    return result;
+}
+
+int len = prompt("Insert the length of the array: ");
+int min = prompt("Insert the bottom bound of the array: ");
+int max = prompt("Insert the upper bound the array: ");
+
+int [] myarray = RandomArray (len, min, max);
+PrintArray(myarray);
+Console.WriteLine($"The total number of even number in the array is {NumberOfEven(myarray)}");
