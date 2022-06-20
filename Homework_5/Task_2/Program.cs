64add4e5711f6b84b993881da180f71f4c312f6e
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-20 23:06:38 +0300

    took everything from task 1

--- Homework_5/Task_2/Program.cs
+++ Homework_5/Task_2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int prompt (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int [] RandomArray (int len, int min, int max)
+{
+    
+    int [] RngArray = new int [len];
+    for (int i =0; i<RngArray.Length;i++)
+    RngArray[i]= new Random().Next(min,max+1);
+    return RngArray;
+}
+void PrintArray (int [] array)
+{
+    Console.Write("[");
+    for (int i=0; i<array.Length-1; i++)
+    {
+        Console.Write(array[i]+", ");
+    }
+    Console.WriteLine(array[array.Length-1]+"]");
+}
+int SumOfOddPos (int [] array)
+{
+    int result=0;
+    for (int i=0; i<array.Length; i=i+2)
+    result=result+array[i];
+    return result;
+}
+
+int len = prompt("Insert the length of the array: ");
+int min = prompt("Insert the bottom bound of the array: ");
+int max = prompt("Insert the upper bound the array: ");
+int [] myarray = RandomArray (len, min, max);
+PrintArray(myarray);
+Console.WriteLine($"The total sum of numbers on odd position in the array is {SumOfOddPos(myarray)}");
