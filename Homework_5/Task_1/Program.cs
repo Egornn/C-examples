49a877948d71aee21600bac526af5af9760cec3b
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-20 22:43:52 +0300

    two methods

--- Homework_5/Task_1/Program.cs
+++ Homework_5/Task_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int prompt (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int [] RandomArray (int len, int min, int max)
+{
+    
+    int [] RngArray = new int [len];
+    for (int i =0; i<RngArray.Length;i++)
+    RngArray[i]= new Random().Next(min,max);
+    return RngArray;
+}
