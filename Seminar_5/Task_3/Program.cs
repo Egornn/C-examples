0d2fd8ef91820a9f4351332afa3fcf41f0b190b8
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-20 21:11:22 +0300

    done

---
+++ Seminar_5/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] initArray (int len, int min, int max)
+{
+    Random rnd = new Random();
+    int[] array = new int [len];
+    for (int i =0; i<len; i++)
+    {
+        array[i]= new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int search2digit (int [] array)
+{
+    int result =0;
+    for (int i = 0; i< array.Length;i++)
+    {
+        if (array[i]>=10&&array[i]<=99) result++;
+    }
+    return result;
+}
+
+int [] array=initArray (123,0,123);
+Console.WriteLine(search2digit(array));
