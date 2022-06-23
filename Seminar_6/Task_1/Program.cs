8d5636ab080da07bae327236b0985b8f5e50b72e
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-23 20:39:45 +0300

    inverse array

--- Seminar_6/Task_1/Program.cs
+++ Seminar_6/Task_1/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int [] CreateArray (int len, int min, int max)
+{
+    var array = new int [len];
+    var rnd = new Random();
+    for (int i=0; i<len;i++)
+    {
+        array[i]=rnd.Next(min, max+1);
+    }
+    return array;
+} 
+void PrintArray (int [] array)
+{
+    for (int i=0; i<array.Length-1; i++)
+    {
+        Console.Write(array[i]+", ");
+    }
+    Console.WriteLine(array[array.Length-1]);
+}
+int [] InverseArray (int [] array)
+{
+    int [] reverse = new int [array.Length];
+    for (int i = 0; i<(array.Length);i++)
+    {
+        reverse[i]=array[array.Length-1-i];
+
+    }
+    return reverse;
+}
+
+
+int [] NewArray = CreateArray(4,1,20);
+PrintArray (NewArray);
+PrintArray (InverseArray(NewArray));
