9e40ea13982f9b0d2f662eea3491711b6b9ff538
Egor Nazdryuhin <egornnn@gmail.com>
2022-06-27 22:04:27 +0300

    Double matrix

--- Homework_7/task_1/Program.cs
+++ Homework_7/task_1/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Введите элемент, и найдите первое его вхождение, 
+//выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    string intString = Console.ReadLine();
+    return int.Parse (intString);
+}
+
+void PrintArray (double [,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1);j++)
+        {
+            Console.Write($"{matrix[i,j]:f2}"+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] CreateRandomMatrix (int row, int column, int min, int max)
+{   
+    var rnd=new Random();
+    double [,] newMatrix = new double[row,column];
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<column;j++)
+        {
+            newMatrix[i,j]=rnd.Next(min, max+1)+rnd.NextDouble();
+        }
+    }
+    return newMatrix;
+}
+
+
+int n =Prompt ("Input number of rows ");
+int m =Prompt ("Input number of columns ");
+int min =Prompt ("Input minum ");
+int max =Prompt ("Input maximum ");
+
+
+double [,] myArray = CreateRandomMatrix(n,m,min,max);
+
+PrintArray(myArray);
+
