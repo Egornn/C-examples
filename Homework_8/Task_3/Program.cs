51e8842b70a5f9086eca07598a2bccecea37b0cf
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-01 00:36:37 +0300

    matrix muliplication is working

--- Homework_8/Task_3/Program.cs
+++ Homework_8/Task_3/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int [,] CreateRandomMatrix (int row, int column, int min, int max) //Usual generator of random matrix
+{   
+    var rnd=new Random();
+    int [,] newMatrix = new int[row,column];
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<column;j++)
+        {
+            newMatrix[i,j]=rnd.Next(min, max+1);
+        }
+    }
+    return newMatrix;
+}
+
+void PrintArray (int [,] matrix) // Print array to check the results
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1);j++)
+        {
+            Console.Write($"{matrix[i,j]}"+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyIfPossible (int [,] matrixA, int [,]matrixB) // Check if #columns in first matrix equals #rows in second one
+{
+    if (matrixA.GetLength(1)!=matrixB.GetLength(0)) 
+    {
+        System.Console.WriteLine("It is impossible to muliply");
+    }
+    else
+    {
+        int[,] multiplicative = MatrixMultiplication(matrixA, matrixB);
+        PrintArray(multiplicative);
+    }
+}
+
+int [,] MatrixMultiplication(int[,] matrixA, int [,] matrixB)
+{
+    int[,] multiplicative = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < multiplicative.GetLength(0);i++)
+    {
+        for (int j = 0; j < multiplicative.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1);k++)
+            {
+                multiplicative[i, j] = multiplicative[i, j] + matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return multiplicative;
+}
+
+void Solve()
+{
+    int n = 2;
+    int m = 3;
+    int o = 3;
+    int p = 4;
+
+    int min = 0;
+    int max = 10;
+    int[,] matrixA = CreateRandomMatrix(n, m, min, max);
+    int[,] matrixB = CreateRandomMatrix(o, p, min, max);
+    PrintArray(matrixA);
+    System.Console.WriteLine();
+     PrintArray(matrixB);
+    System.Console.WriteLine();
+    MultiplyIfPossible(matrixA, matrixB);
+
+}
+
+Solve();
