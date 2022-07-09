de4b441baf226dc8161d87cdc2d0aef26cdc75f5
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 22:06:13 +0300

    done

--- Seminar_9/task_5/Program.cs
+++ Seminar_9/task_5/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Is power of 2
+
+string RecursiveIfPowerOftwo(double N, int B)
+{
+    
+    if (N<=2)
+    {
+        if (N==2)    
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
+    else
+    {
+        
+        return RecursiveIfPowerOftwo(N/2, B);
+    }
+}
+
+double Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return double.Parse(numberStr);
+}
+
+void Solve()
+{
+    double m = Prompt("Insert the number to check if its 2^k");
+    int n = 2;
+    System.Console.WriteLine( RecursiveIfPowerOftwo(m,n));
+
+}
+
+Solve();
+
