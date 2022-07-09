40b14c0ce65d7f837da9220aaae1a821e23b7726
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 21:52:47 +0300

    Done

--- Seminar_9/task_3/Program.cs
+++ Seminar_9/task_3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сумма чисел 
+
+int RecursiveDigitSum (int N)
+{
+    int num = 0;
+    if (N == 0)
+    { 
+        return 0; 
+    }
+    else
+    {
+        num = N % 10 + RecursiveDigitSum(N / 10);
+        return num;
+    }
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Insert number to find sum of its digits");
+    System.Console.WriteLine( RecursiveDigitSum(m));
+
+}
+
+Solve();
+
