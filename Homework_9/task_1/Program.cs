f1d5fabc2eb2b891582359137288448f54ab71cc
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 22:29:35 +0300

    done like the task_2 from seminar

--- Homework_9/task_1/Program.cs
+++ Homework_9/task_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Even from M to N
+void RecursiveMToN (int M, int N )
+{
+    if (N<M) 
+    {
+        return;
+    }
+    else
+    {
+        {
+            if (M % 2 == 0)
+            {
+                System.Console.Write($"{M}, ");
+            }
+            RecursiveMToN(M + 1, N);
+        }
+    }
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Insert bottom boundary");
+    int n = Prompt("Insert upper boundary");
+    RecursiveMToN(m,n);
+
+}
+
+
+
+Solve();
