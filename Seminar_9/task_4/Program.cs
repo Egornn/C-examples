e0de5c4986f50557e4c097486bea47a2d9abfd9c
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 21:57:55 +0300

    done

--- Seminar_9/task_4/Program.cs
+++ Seminar_9/task_4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// A^B
+
+int RecursiveAToPowerB(int A, int B)
+{
+    int num = 1;
+    if (B == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        num = A * RecursiveAToPowerB(A, B - 1);
+        return num;
+    }
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Insert the base");
+    int n = Prompt("Insert the power");
+    System.Console.WriteLine( RecursiveAToPowerB(m,n));
+
+}
+
+Solve();
+
