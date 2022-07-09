432eee3a89b2e8bc36e11fa7c172e75aa5a016e7
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 21:34:09 +0300

    done

--- Seminar_9/Task_1/Program.cs
+++ Seminar_9/Task_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Натуральные от 1 до N рекурсией
+
+void Recursive1ToN (int N)
+{
+    if (N<1) 
+    {
+        return;
+    }
+    else
+    {
+        Recursive1ToN(N-1);
+        System.Console.Write($"{N}, ");
+        
+    }
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    
+    int n = Prompt("Insert upper boundary");
+    Recursive1ToN(n);
+
+}
+
+
+
+Solve();
