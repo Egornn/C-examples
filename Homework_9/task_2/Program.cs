ecb1c3367a39949b130e8139595c83f5ab55b3f6
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-09 22:32:22 +0300

    done in similar way

--- Homework_9/task_2/Program.cs
+++ Homework_9/task_2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Sum from M to N
+int RecursiveMToNSum (int M, int N )
+{
+    int total = 0;
+    if (N<M) 
+    {
+        return 0;
+    }
+    else
+    {
+        {
+            total=M+RecursiveMToNSum(M + 1, N);
+            return total;
+        }
+    }
+
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Insert bottom boundary");
+    int n = Prompt("Insert upper boundary");
+    System.Console.WriteLine( RecursiveMToNSum(m,n));
+
+}
+
+
+
+Solve();
