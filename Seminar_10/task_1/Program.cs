d0bed49737d391048f303dce5ebca1e1c0b4122a
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-10 08:11:09 +0300

    done

--- Seminar_10/task_1/Program.cs
+++ Seminar_10/task_1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  N чисел, для которых каждое следующее равно сумме двух предыдущих.
+
+
+void RecursiveFibonacciMToN (int firstN, int secondN, int number )
+{
+    if (number==2) 
+    {
+        System.Console.WriteLine($"{firstN}, {secondN}.");
+    }
+    else
+    {
+        {
+            
+            System.Console.Write($"{firstN}, ");
+            RecursiveFibonacciMToN(secondN, firstN+secondN, number-1);
+            
+        }
+    }
+}
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int first = Prompt("Insert first number");
+    int second = Prompt("Insert second number");
+    int howMany = Prompt("How many numbers do you want? ");
+    RecursiveFibonacciMToN(first,second,howMany);
+
+}
+
+
+
+Solve();
