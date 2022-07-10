3c0d9cbdb3dbdd094ed4aed4240425f095affb78
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-10 09:45:47 +0300

    done

--- Homework_10/task_1/Program.cs
+++ Homework_10/task_1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// i times i up to N
+using System.Text;
+void RecursiveNRepeated(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    else
+    {
+        {
+            string str = new StringBuilder($"{N} ".Length * N).Insert(0, $"{N} ", N).ToString();
+            RecursiveNRepeated(N-1);
+            System.Console.Write(str);            
+        }
+    }
+}
+
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int N = Prompt("Insert the number");
+    RecursiveNRepeated(N);
+
+}
+
+
+
+Solve();
