65310314f99c4a27e0539979e5ddacbd409f002e
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-10 09:18:50 +0300

    done

--- Seminar_10/task_3/Program.cs
+++ Seminar_10/task_3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Дано натуральное число N. Выведите все его цифры по одной, в обратном порядке, разделяя их пробелами или новыми строками.
+
+void RecursiveNReversed (int N)
+{
+    if (N==0) 
+    {
+            System.Console.WriteLine();        
+    }
+    else
+    {
+        {
+            Console.Write($"{N%10} ");
+            RecursiveNReversed(N/10);
+        }
+    }
+}
+
+
+int Prompt (string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int N = Prompt("Insert the number");
+    RecursiveNReversed(N);
+
+}
+
+
+
+Solve();
